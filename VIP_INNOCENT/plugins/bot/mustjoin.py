9f9c73f47f9a19af91d5f3c265088848572e3690
Sandeep sharma <rs3268840@gmail.com>
2025-08-06 11:17:38 +0530

    Update mustjoin.py

--- VIP_INNOCENT/plugins/bot/mustjoin.py
+++ VIP_INNOCENT/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from VIP_INNOCENT import app
 
 #--------------------------
 
-MUST_JOIN = "BotzEmpire"
+MUST_JOIN = "bots_update_all"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
